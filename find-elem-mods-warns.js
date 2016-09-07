5668b6b691c17da007ddba966053e995a94f99e3
Александр invntrm@ <a-x-@users.noreply.github.com>
2016-09-07 14:08:09 +0400

    Create find-elem-mods-warns.js

---
+++ find-elem-mods-warns.js
@@ -0,0 +1,17 @@
+#!/usr/env node
+// env: VERBOSE
+// usage: VERBOSE=1 node find-elem-mods-warns.js | less
+// usage: subl -n $(node find-elem-mods-warns.js)
+const findAstObjects = require('./find-ast-objects')
+
+const globPattern = '*/blocks-*/**/*.js'
+
+const findElemModsWarns = module.exports = function findElemModsWarns(globPattern) {
+    findAstObjects(node => {
+        if (_(node.properties).map('key.name').intersection(['elem', 'mods']).size() !== 2) return false
+        if (_(node.properties).map('key.name').includes('elemMods')) return false
+        return true
+    })
+}
+
+module.parent || findElemModsWarns(globPattern)
