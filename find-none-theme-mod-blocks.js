9fcdd12a635d8d78bed066240f35b2ba1b41124f
Александр invntrm@ <a-x-@users.noreply.github.com>
2016-09-07 14:04:09 +0400

    Create find-none-theme-mod-blocks.js

---
+++ find-none-theme-mod-blocks.js
@@ -0,0 +1,19 @@
+#!/usr/env node
+// env: VERBOSE
+// usage: VERBOSE=1 node find-none-theme-mod-blocks.js | less
+// usage: subl -n $(node find-none-theme-mod-blocks.js)
+
+const globPattern = '*/blocks-*/**/*.js'
+const themeDepsBlocks = [
+    'button',
+    'button2',
+    'check-button',
+    'link',
+    'radio-button',
+    'radiobox',
+    'tabs',
+    'tumbler',
+]
+const targetMod = 'theme'
+
+require('./find-mods-errors')(globPattern, themeDepsBlocks, targetMod)
