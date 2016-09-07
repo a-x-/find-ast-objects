cebf422e2a6cd42bb9e27f81737e463b803d6802
Александр invntrm@ <a-x-@users.noreply.github.com>
2016-09-07 14:08:30 +0400

    Create find-target-attrs-errors.js

---
+++ find-target-attrs-errors.js
@@ -0,0 +1,32 @@
+#!/usr/env node
+// env: VERBOSE
+// usage: VERBOSE=1 node find-elem-mods-warns.js | less
+// usage: subl -n $(node find-elem-mods-warns.js)
+const findAstObjects = require('./find-ast-objects')
+
+const globPattern = '*/blocks-*/**/*.js'
+const targetBlocks = [
+    'button',
+    'button2',
+    'link',
+]
+const target = 'target'
+
+const findTargetAttrsErrors = module.exports = function findTargetAttrsErrors(globPattern, targetBlocks, target) {
+    findAstObjects(node => {
+        if (!node.properties.filter(p => p.key.name === 'block' && _.includes(targetBlocks, p.value.value)).length) return false
+        if (!_(node.properties).map('key.name').includes('attrs')) return false
+
+        const hasAttrsTarget = _.chain(node.properties)
+            .find(['key.name', 'attrs'])
+            .get('value.properties')
+            .find(['key.name', target])
+            .value()
+
+        if (!hasAttrsTarget) return false
+
+        return true
+    })
+}
+
+module.parent || findTargetAttrsErrors(globPattern, targetBlocks, target)
