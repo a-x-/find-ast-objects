10972db8f8d4d351e4c9db6628126bcd0aba54e0
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-10-11 02:22:46 +0300

    Move plugins/v2 plugins/{bem,bemxjst}. Add initial bin/fa. Add dev-depends

---
+++ bin/fa.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// fa plugin [--level warn..error] [--verbose 1..2]
+// fa --config .fa.json5
+
+// todo
--- plugins/tests.js
+++ plugins/tests.js
@@ -1,21 +1,24 @@
 const test = require('./common/test')
 const partial = require('lodash/partial')
 
+// fixme: full test coverage
 describe('plugins', () => {
-    describe('mods', () => {
-        describe('theme', () => {
-            const testTheme = partial(test, require('./v2/find-none-theme-mod-blocks'))
+    describe('bemxjst6', () => {
+        describe('mods', () => {
+            describe('theme', () => {
+                const testTheme = partial(test, require('./bemxjst6/find-none-theme-mod-blocks'))
 
-            it('empty mods', () => testTheme('theme.01.js', {}, true))
-            it('w/o mods', () => testTheme('theme.02.js', {}, true))
-            it('with theme', () => testTheme('theme.03.js', {}, false))
-            it('not target object', () => testTheme('theme.04.js', {}, false))
-            it('mods variable', () => testTheme('theme.05.js', {}, true))
-            it('mods variable strict', () => testTheme('theme.05.js', {level: 'error'}, false))
-            it('block mods elem', () => testTheme('theme.06.js', {}, true))
-            it('block elem', () => testTheme('theme.07.js', {}, false))
-            it('block mods with theme elem', () => testTheme('theme.08.js', {}, false))
-            it('elem mods', () => testTheme('theme.09.js', {}, false))
+                it('empty mods', () => testTheme('theme.01.js', {}, true))
+                it('w/o mods', () => testTheme('theme.02.js', {}, true))
+                it('with theme', () => testTheme('theme.03.js', {}, false))
+                it('not target object', () => testTheme('theme.04.js', {}, false))
+                it('mods variable', () => testTheme('theme.05.js', {}, true))
+                it('mods variable strict', () => testTheme('theme.05.js', {level: 'error'}, false))
+                it('block mods elem', () => testTheme('theme.06.js', {}, true))
+                it('block elem', () => testTheme('theme.07.js', {}, false))
+                it('block mods with theme elem', () => testTheme('theme.08.js', {}, false))
+                it('elem mods', () => testTheme('theme.09.js', {}, false))
+            })
         })
     })
 })
