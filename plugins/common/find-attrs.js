462327ae44392e97e731324dee6c909e043642f6
Александр invntrm@ <a-x-@users.noreply.github.com>
2016-10-12 14:24:33 +0300

    Update find-attrs.js

--- plugins/common/find-attrs.js
+++ plugins/common/find-attrs.js
@@ -4,6 +4,8 @@ const hasAttr = require('../common/has-attr')
 const getBlockName = require('./block-name')
 
 module.exports = function findAttrsErrors(targetBlocks, target) {
+    // @param {AcornESTreeNode} node
+    // @see estree spec https://github.com/estree/estree
     return node => {
         if (!_.includes(targetBlocks, getBlockName(node))) return false
         if (!_(node.properties).map('key.name').includes('attrs')) return false
