57177b87d1fb6aa287c9b660afcb0da1a377416f
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2017-02-13 16:29:44 +0300

    add find elem

--- plugins/find-block.js
+++ plugins/find-block.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 // env: VERBOSE
 // usage: find-block.js '*/blocks-*/**/*.js' block-name
+// usage: find-block.js '*/blocks-*/**/*.js' suggest2-form__node
+
+// todo: fix find block only, check rev deps, add tests
+
 const _ = require('lodash')
 const findAstObjects = require('../find-ast-objects')
+const parseBem = bemslug => require('bem-naming').parse(bemslug)
 
 const getMod = (node, mod) => _.chain(node.properties)
     .find(['key.name', 'mods'])
@@ -17,10 +22,19 @@ const isModsPlainObject = (node) => _.chain(node.properties)
 
 const globPattern = process.argv[2] || '*/blocks-*/**/*.js'
 
-const findBlock = module.exports = function findBlock(globPattern, targetBlock) {
+const hasPropWithNameVal = (node, name, val) => node.properties.filter(p => p.key.name === name && p.value.value === val).length
+
+/**
+ * @prop {String} globPattern
+ * @prop {String} targetBlock
+ * @prop {?String} targetElem
+ */
+const findBlock = module.exports = function findBlock(globPattern, targetBlock, targetElem) {
     findAstObjects(globPattern, node => {
-        return node.properties.filter(p => p.key.name === 'block' && p.value.value === targetBlock).length
+        return hasPropWithNameVal(node, 'block', targetBlock)
+            //&& (targetElem ? hasPropWithNameVal(node, 'elem', targetElem) : false)
     })
 }
 
-module.parent || findBlock(globPattern, process.argv[3])
+const bem = parseBem(process.argv[3])
+module.parent || findBlock(globPattern, bem.block, bem.elem, bem.modName, bem.modVal)
